f1659b6df4888b17bc3d7f86e7d8b6760127a62a
Thomas <Thomas.Robert.Tennoy@stud.hials.no>
2015-10-19 11:31:52 +0200

    addArtikkel

---
+++ website/addArtikkel.php
@@ -0,0 +1,41 @@
+
+
+	
+<?php
+$serverName = "(local)"; // eller "(local)"
+$connectionInfo = array( "Database"=>"musikkavis", "UID"=>"test", "PWD"=>"123", "CharacterSet" => "UTF-8");
+$conn = sqlsrv_connect( $serverName, $connectionInfo );
+if( $conn === false ) {
+    die( print_r( sqlsrv_errors(), true));
+}
+
+//print_r($_POST);
+
+$query = "INSERT INTO artikkel
+           (tittel
+           ,tekst
+           ,ingress
+           ,a_typeID)
+     VALUES
+           (?,?,?,?)";
+		  
+		   
+		   $params = array($_POST['tittel'],$_POST['tekst'],$_POST['ingress'],$_POST['a_typeID']);
+$stmt = sqlsrv_query($conn,$query,$params);
+
+
+
+if ( $stmt === false ) {
+   echo "Error in statement preparation/execution.\n";
+   die( print_r( sqlsrv_errors(), true));
+} else{
+	echo "Rows affected: ".sqlsrv_rows_affected( $stmt )."\n";
+}
+
+sqlsrv_fetch( $stmt );
+
+
+
+sqlsrv_close( $conn);
+?>
+	
