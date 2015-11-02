4d99c371fe477ba2f5fe8eb5ed0e0e2181427f5d
ghp91 <gaaute@hotmail.com>
2015-11-02 12:19:27 +0100

    minside prøving

--- website/minside.php
+++ website/minside.php
@@ -8,13 +8,13 @@ include_once('/phplogic/restrictAccess.php');
 if(isAny()){}
 else{header('Location: login.php');}
 ?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
+    
+  
 <?php headTag();?>
-  <p>Dette er min side. Her kan du finne informasjon om din bruker</p>
-<!---------------------------------Content Item starts here-------------------------------------------->
-
-<!------------------------------------------And ends here---------------------------------------->
- 
+    
+    <p>fdsfosdfosd</p>
 <?php content();//Also contains a refference to the separate sidebar file ?>
 </html>
