9fd8b4f55613ff9f22a47bdce604f336cc2f1e6a
Thomas <Thomas.Robert.Tennoy@stud.hials.no>
2015-11-10 10:01:32 +0100

    lagt til usertype 0 i skript

--- Database Scriptfiler/5 - Add Demo Users.sql
+++ Database Scriptfiler/5 - Add Demo Users.sql
@@ -1,6 +1,9 @@
 use musikkavis
 
 INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
+VALUES ('user@tungrocken.no','userpass','User','User',GETDATE(),GETDATE(),0)
+
+INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
 VALUES ('admin@tungrocken.no','adminpass','Admin','Admin',GETDATE(),GETDATE(),1)
 
 INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
