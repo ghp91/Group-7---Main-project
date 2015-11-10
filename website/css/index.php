907dc5d41c9d8904b06dce738513127488a1dabd
Sindre Sjøholt <sindre@sjoholt.net>
2015-11-10 08:57:21 +0100

    Redirect-filer i undermapper.
    Hindrer tilgang til undermapper via nettleser

---
+++ website/css/index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<script language=javascript>
+function redirect(){
+  window.location = "../index.php";
+}
+</script>
+<body onload="redirect()">
+
+</body>
+</html>
