ece6f1ab04f96486f65b6790bc7dc7fa82c3840c
Sindre Sjøholt <sindre@sjoholt.net>
2015-11-10 09:24:20 +0100

    Edit User knapp på admin.php
    linker til edituser.php

--- website/admin.php
+++ website/admin.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 session_start();
 include_once('/phplogic/connection.php');
 include_once('/structure/header.php');
@@ -19,12 +19,14 @@ else{header('Location: login.php');}
 <?php if(isAdmin()){echo'
           <p><a href="adduser.php"><img src="images/adduser.jpg" width="400" height="150" alt=""/></a>
           <p><br>';
+          <p><a href="edituser.php"><img src="images/edituser.jpg" width="400" height="150" alt=""/></a>
+          <p><br>';
 }?>
           <p><a href="addarticle.php"><img src="images/addarticle.jpg" width="400" height="150" alt=""/></a>
           <p><br>
           <p><a href="deletearticle.php"><img src="images/deletearticle.jpg" width="400" height="150" alt=""/></a>
           <p><br>
 <!------------------------------------------And ends here---------------------------------------->
- 
+
 <?php content();//Also contains a refference to the separate sidebar file ?>
 </html>
