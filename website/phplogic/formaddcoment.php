e694958ed4a3ad09e1a481105f5bbc307f76e64f
Thomas <Thomas.Robert.Tennoy@stud.hials.no>
2015-11-11 11:56:31 +0100

    fjernet unødvendige echo etc

--- website/phplogic/formaddcoment.php
+++ website/phplogic/formaddcoment.php
@@ -5,7 +5,6 @@ include_once('/connection.php');
 ?>
 	
 <?php
-//print_r($_POST);
 $datePosted =date('Y-m-d G:i:s');
 $query = "INSERT INTO kommentar
            (tittel
@@ -13,18 +12,16 @@ $query = "INSERT INTO kommentar
            ,artikkelID
 		   ,dateComented)
      VALUES
-           (?,?,?,?); SELECT SCOPE_IDENTITY()";
-		  
+           (?,?,?,?); SELECT SCOPE_IDENTITY()";		  
 		   $aid = $_GET["id"];
 		   $params = array($_POST['tittel'],$_POST['tekst'],$aid,$datePosted);
 $stmt = sqlsrv_query($conn,$query,$params);
-echo $_POST['aid'];
 
 if ( $stmt === false ) {
    echo "Error in statement preparation/execution.\n";
    die( print_r( sqlsrv_errors(), true));
 } else{
-echo "Rows affected: ".sqlsrv_rows_affected( $stmt )."\n";}
+
 //---------------------------artikkelRelasjon-----------------------------//
 $epost = $_SESSION['epost'];
 $author = true;
@@ -32,8 +29,6 @@ $like = false;
 sqlsrv_next_result($stmt); 
 sqlsrv_fetch($stmt); 
 $kommentarID = sqlsrv_get_field($stmt, 0);
-echo $kommentarID;
-//$kommentarID = intval(sqlsrv_query($conn,"SELECT TOP 1 kommentarID FROM kommentar ORDER BY convert(datetime, dateComented, 103) DESC"));
 $query2 = "INSERT INTO kommentar_bruker
            (e_mail
 		   ,kommentarID
@@ -46,17 +41,11 @@ $query2 = "INSERT INTO kommentar_bruker
 $stmt2 = sqlsrv_query($conn,$query2,$params2);
 //---------------------------end of artikkelRelasjon---------------------//
 
-
-
 if ( $stmt2 === false ) {
    echo "Error in statement preparation/execution.\n";
    die( print_r( sqlsrv_errors(), true));
 } else{
-	echo "Rows affected: ".sqlsrv_rows_affected( $stmt )."\n";
 	$location = "Location: /article.php?id=".$aid;
         header($location);
-    
 }
-
-sqlsrv_fetch( $stmt );
 ?>
