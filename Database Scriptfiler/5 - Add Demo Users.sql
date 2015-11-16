97fc28e41072d6205ba8fbc26b6b6125fd8890a7
Sindre Sjøholt <sindre@sjoholt.net>
2015-11-16 13:32:25 +0100

    Endret subscriber i scriptet, lagt til 365 dager

--- Database Scriptfiler/5 - Add Demo Users.sql
+++ Database Scriptfiler/5 - Add Demo Users.sql
@@ -1,13 +1,13 @@
 use musikkavis
 
-INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
+INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype)
 VALUES ('user@tungrocken.no','userpass','User','User',GETDATE(),GETDATE(),0)
 
-INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
+INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype)
 VALUES ('admin@tungrocken.no','adminpass','Admin','Admin',GETDATE(),GETDATE(),1)
 
-INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
+INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype)
 VALUES ('journalist@tungrocken.no','journalistpass','Journalist','Journalist',GETDATE(),GETDATE(),2)
 
-INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype) 
-VALUES ('subscriber@tungrocken.no','subscriberpass','Subscriber','Subscriber',GETDATE(),GETDATE(),3)
+INSERT INTO bruker  (e_mail,passord,fornavn,etternavn,registered,sub_expire,utype)
+VALUES ('subscriber@tungrocken.no','subscriberpass','Subscriber','Subscriber',GETDATE(),GETDATE()+365,3)
